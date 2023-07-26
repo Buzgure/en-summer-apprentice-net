87ba2bb5c79109c848a1375090d88c3bfb17ce0f
Buzgure <tudor.buzgure@stud.ubbcluj.ro>
2023-07-26 19:26:11 +0300

    implemented orderDTO and orderRepo

---
+++ TMS.API/Model/Dto/OrderDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace TMS.API.Model.Dto
+{
+    public class OrderDTO
+    {
+        public long Id { get; set; }
+
+        public long TicketCategoryId { get; set; }
+
+        public string CustomerName { get; set; }
+        
+        public DateTime? OrderedAt { get; set; }
+
+        public int NumberOfTickets { get; set; }
+
+        public float TotalPrice { get; set; }
+        
+    }
+}
---
+++ TMS.API/Repository/IOrderRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace TMS.API.Repository
+{
+    public interface IOrderRepository
+    {
+    }
+}
